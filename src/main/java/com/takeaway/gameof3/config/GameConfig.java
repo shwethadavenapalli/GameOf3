0e01f21c70d91b82cd9095051171a6b075a0f790
shwethadavenapalli <shwetha.davenapalli@gmail.com>
2018-12-08 22:53:47 +0100

    added conditional bean

--- src/main/java/com/takeaway/gameof3/config/GameConfig.java
+++ src/main/java/com/takeaway/gameof3/config/GameConfig.java
@@ -1,28 +1,36 @@
 package com.takeaway.gameof3.config;
 
 import com.takeaway.gameof3.domain.GameInitiator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by Shwetha on 08-12-2018.
  */
 @Configuration
 public class GameConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(GameConfig.class);
+
     @Bean
     public TaskExecutor taskExecutor() {
         return new SimpleAsyncTaskExecutor();
     }
 
+    @ConditionalOnProperty("game.initiator")
     @Bean
     public CommandLineRunner schedulingRunner(TaskExecutor executor) {
         return new CommandLineRunner() {
             public void run(String... args) throws Exception {
-                executor.execute(new GameInitiator());
+                executor.execute(
+                        new GameInitiator());
             }
         };
     }
--- src/main/java/com/takeaway/gameof3/domain/GameInitiator.java
+++ src/main/java/com/takeaway/gameof3/domain/GameInitiator.java
@@ -4,6 +4,7 @@ package com.takeaway.gameof3.domain;
  * Created by Shwetha on 08-12-2018.
  */
 public class GameInitiator implements Runnable {
+
     @Override
     public void run() {
         System.out.println("Inside run!!");
