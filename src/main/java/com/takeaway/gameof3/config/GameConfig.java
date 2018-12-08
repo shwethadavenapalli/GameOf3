073614f56a3d81b8ff7bbb15de6db9a639cae0d9
shwethadavenapalli <shwetha.davenapalli@gmail.com>
2018-12-09 00:10:27 +0100

    Refactored GameConfig to have a bean defn for GameInitiator and RestTemplate

--- src/main/java/com/takeaway/gameof3/config/GameConfig.java
+++ src/main/java/com/takeaway/gameof3/config/GameConfig.java
@@ -3,13 +3,13 @@ package com.takeaway.gameof3.config;
 import com.takeaway.gameof3.domain.GameInitiator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Shwetha on 08-12-2018.
@@ -20,17 +20,25 @@ public class GameConfig {
     private static final Logger log = LoggerFactory.getLogger(GameConfig.class);
 
     @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
     public TaskExecutor taskExecutor() {
         return new SimpleAsyncTaskExecutor();
+    }
+    @Bean
+    public GameInitiator getGameInitiator(RestTemplate restTemplate){
+        return new GameInitiator(restTemplate);
     }
 
     @ConditionalOnProperty("game.initiator")
     @Bean
-    public CommandLineRunner schedulingRunner(TaskExecutor executor) {
+    public CommandLineRunner schedulingRunner(TaskExecutor executor, GameInitiator gameInitiator) {
         return new CommandLineRunner() {
             public void run(String... args) throws Exception {
-                executor.execute(
-                        new GameInitiator());
+                executor.execute(gameInitiator);
             }
         };
     }
